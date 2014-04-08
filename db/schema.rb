333d5e13770624fb3ae51ea57b83da1245bc64f3
nathanhamilton <esquireofthelord@gmail.com>
2014-04-08 11:45:23 -0500

    Change database configuration to work with pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140326202131) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: true do |t|
     t.text     "content"
     t.integer  "deck_id"
